1979402ef1e1d390363c4d092542611b4f5d6ef2
A B <securionaire@protonmail.com>
2018-12-11 23:24:54 -0500

    Added comments and implemented balanceOf function

--- contracts/Tokvent.sol
+++ contracts/Tokvent.sol
@@ -2,14 +2,24 @@ pragma solidity ^0.4.0;
 
 contract Tokvent {
 
+    // Total supply of all Tokvent tokens minted.
     uint _totalSupply;
+
+    // Hash map containing information of all balances of all token holders.
+    mapping (address => uint) _balances;
 
     constructor () {
         
     }
 
+    // Function that returns total supply of all tokens minted.
     function totalSupply () public view returns (uint) {
         return _totalSupply;
+    }
+
+    // Function that returns balance of a particular token owner.
+    function balanceOf (address tokenOwner) public view returns (uint balance) {
+        return _balances[tokenOwner];
     }
 
 }
