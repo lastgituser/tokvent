23a760fbde439f42ae10e0f8fbe4db9de366d975
A B <securionaire@protonmail.com>
2018-12-11 23:37:38 -0500

    Changed formatting a bit and added allowance function

--- contracts/Tokvent.sol
+++ contracts/Tokvent.sol
@@ -8,18 +8,32 @@ contract Tokvent {
     // Hash map containing information of all balances of all token holders.
     mapping (address => uint) _balances;
 
+    // Hash map containing information about how much money the token owner has
+    // allowed the spender to place into the spender's own account.
+    mapping (address => (address => uint)) _allowances;
+
     constructor () {
         
     }
 
     // Function that returns total supply of all tokens minted.
-    function totalSupply () public view returns (uint) {
+    function totalSupply () 
+                        public view returns (uint) {
         return _totalSupply;
     }
 
     // Function that returns balance of a particular token owner.
-    function balanceOf (address tokenOwner) public view returns (uint balance) {
+    function balanceOf (address tokenOwner) 
+                        public view returns (uint balance) {
         return _balances[tokenOwner];
+    }
+
+    // Function that returns amount that the tokenOwner has allowed the spender
+    // to take from the tokenOwner's account and place into the spender's
+    // account.
+    function allowance (address tokenOwner, address spender) 
+                        public view returns (uint remaining) {
+        return _allowances[tokenOwner][spender];
     }
 
 }
